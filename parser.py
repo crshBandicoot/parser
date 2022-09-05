d06a328b340d1ab719d86f0025f2e8f168cb775e
Bogdan Pizhuk <bogdanpizuk2604@gmail.com>
2022-09-06 01:54:38 +0300

    steam new games

--- parser.py
+++ parser.py
@@ -1,40 +1,58 @@
+from turtle import st
 from requests import get
 from bs4 import BeautifulSoup
 import json
 import unicodedata
 import re
 
-
-def slugify(value, allow_unicode=False): #title to valid filename
+def slugify(value, allow_unicode=False):  # title to valid filename
     value = str(value)
     if allow_unicode:
         value = unicodedata.normalize('NFKC', value)
     else:
-        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+        value = unicodedata.normalize('NFKD', value).encode(
+            'ascii', 'ignore').decode('ascii')
     value = re.sub(r'[^\w\s-]', '', value.lower())
     return re.sub(r'[-\s]+', '-', value).strip('-_')
 
 
 def jsonify(url, filter):
 
+    json_array = []
+
     page = BeautifulSoup(get(url).text, 'lxml')
-    title = page.find(id='firstHeading').get_text()
-    body = page.find(id='bodyContent').find('p').find_next_sibling('p').get_text()
-    filtered = True
+    products = page.find(id='tab_newreleases_content', class_='tab_content').find_all('a', class_='tab_item')
 
-    for el in filter:
-        if el.lower() not in title.lower() and el.lower() not in body.lower():
-            filtered = False
-            break
-    
-    if filtered:
-        with open(slugify(title) + '.json', 'w') as json_file:
-            json.dump({'url': url, 'title': title, 'body': body[0:100]+'...'}, json_file, indent=4)
+    for product in products:
+        game_page = BeautifulSoup(get(product['href']).text, 'lxml')
+        title = game_page.find(id='appHubAppName').get_text()
+        body = game_page.find(class_='game_description_snippet').get_text(separator=' ', strip=True)
+        filtered = True
+
+        for el in filter:
+            if el.lower() not in title.lower() and el.lower() not in body.lower():
+                filtered = False
+                break
+
+        if filtered:
+            splitted = body.split()
+
+            if len(splitted) > 100:
+                body = ''
+                for i in range(100):
+                    body += splitted[i] + ' '
+                body = body.rstrip() + '...'
+            
+            json_array.append({'url': product['href'], 'title': title, 'body': body})
+
+    with open(slugify(url) + '.json', 'w+') as json_file:
+        json_file.write(json.dumps(json_array, indent=4))
     
 
-filter = ['rock band', 'Lemmy']
-url = 'https://en.wikipedia.org/wiki/Mot%C3%B6rhead'
+    
+
+
+filter = []
+url = 'https://store.steampowered.com/explore/new/'
 
 jsonify(url, filter)
-
-
